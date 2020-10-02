47e5795c38c78c3299ca7a1f2e7ba3c139854a2c
solitaryewe <58419550+solitaryewe@users.noreply.github.com>
2020-10-01 23:31:34 -0700

    WeSplit (Project 1)

---
+++ Project-01/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,61 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Woolly on 9/26/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var orderAmount: Double { Double(checkAmount) ?? 0 }
+    var tipValue: Double {
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        return orderAmount * (tipSelection / 100)
+    }
+    var grandTotal: Double { return tipValue + orderAmount }
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        return grandTotal / Double(peopleCount)
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Your Order")) {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<25) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section(header: Text("Tip Amount")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section() {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }.navigationBarTitle("WeSplit")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project-01/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Woolly on 9/26/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

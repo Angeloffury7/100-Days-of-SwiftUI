1cfe32eea9326d08145f1e2966d73c705e895c81
solitaryewe <58419550+solitaryewe@users.noreply.github.com>
2020-10-13 16:54:45 -0700

    Project 4 - Challenges Complete

--- Project-04/BetterRest/BetterRest/ContentView.swift
+++ Project-04/BetterRest/BetterRest/ContentView.swift
@@ -16,74 +16,79 @@ struct ContentView: View {
     
     static var defaultWakeTime: Date {
         var components = DateComponents()
+        // Much better wake up time! :)
         components.hour = 10
         components.minute = 0
         return Calendar.current.date(from:components) ?? Date()
     }
     
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
-    
     
     var body: some View {
         NavigationView {
             Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
+                // Challenge 1: change VStacks to Sections.
+                Section(header: Text("When do you want to wake up?").font(.subheadline)) {
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                         .labelsHidden()
                         // Wheel only on iOS.
-                        // Default is fine, bug no longer presenting.
                         .datePickerStyle(WheelDatePickerStyle())
                 }
             
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep:")
-                        .font(.headline)
+                Section(header: Text("Desired amount of sleep:").font(.subheadline)) {
                     Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
                         Text("\(sleepAmount, specifier: "%g") hours")
                     }
                 }
-
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake:")
-                        .font(.headline)
-                    Stepper(value: $coffeeAmount, in: 1...20) {
+                
+                Section(header: Text("Daily coffee intake:").font(.subheadline)) {
+                    // I think this is more visually appealing than the result from Challenge 2, so I left it.
+                    Stepper(value: $coffeeAmount, in: 0...20) {
+                        // Changed range to start at 0.
                         Text("\(coffeeAmount) cup\(coffeeAmount == 1 ? "" : "s")")
                     }
+                    
+                    // Challenge 2: use Picker instead of Stepper for number of cups of coffee.
+//                    Picker("Cups of coffee per day", selection: $coffeeAmount) {
+//                        ForEach(0...20, id: \.self) { coffeeAmount in
+//                            Text("\(coffeeAmount) cup\(coffeeAmount == 1 ? "" : "s")")
+//                        }
+//                    }
+//                    .labelsHidden()
+//                    .pickerStyle(WheelPickerStyle())
+                }
+                
+                Section(header: Text("Your ideal bedtime:").font(.subheadline)) {
+                    // Challenge 3: UI always shows bedtime.
+                    // "When the state value changes, the view invalidates its appearance and recomputes the body."
+                    // Thus the function gets called again and gets to use the new values of the @State variables.
+                    Text(calculateBedtime()).font(.title)
                 }
             }
             .navigationTitle("BetterRest")
-            .navigationBarItems(trailing: Button(action: calculateBedtime) { Text("Calculate")})
-            .alert(isPresented: $showingAlert, content: {
-                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-            })
         }
     }
     
-    private func calculateBedtime() {
+    // Challenge 3: changed return type of function to String, eliminated the Alert.
+    private func calculateBedtime() -> String {
         let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
         let hour = (components.hour ?? 0) * 60 * 60
         let minute = (components.minute ?? 0) * 60
+        
+        var bedtime: String
         
         do {
             let model = try SleepCalculator(configuration: MLModelConfiguration())
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
             let sleepTime = wakeUp - prediction.actualSleep
             
-            alertTitle = "Your ideal bedtime is..."
             let formatter = DateFormatter()
             formatter.timeStyle = .short
-            alertMessage = formatter.string(from: sleepTime)
+            bedtime = formatter.string(from: sleepTime)
         } catch {
             print(error)
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            bedtime = "unable to calculate your bedtime"
         }
-        
-        showingAlert = true
+        return bedtime
     }
 }
 
--- Project-04/README.md
+++ Project-04/README.md
@@ -3,8 +3,11 @@ https://www.hackingwithswift.com/books/ios-swiftui/betterrest-introduction
 
 ## Challenges
 
-Awaiting completion.
+>1. In the Form, replace VStacks with Sections.
+>2. Replace number of cups of coffee Stepper with a Picker.
+>3. Always display and update calculated bedtime in the UI automatically.
 
 ## Screenshots
 
 ![Project 4 - BetterRest](https://github.com/solitaryewe/100-Days-of-SwiftUI/blob/main/Project-04/Screenshots/Project4-large.png)
+![Project 4 - BetterRest](https://github.com/solitaryewe/100-Days-of-SwiftUI/blob/main/Project-04/Screenshots/Project4-challenges-large.png)
